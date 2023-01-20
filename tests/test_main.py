98ed62b0cb0c9a34cfe1abae390f03fa161c7840
12N0c06Izu <ritu6811@gmail.com>
2023-01-20 22:42:06 +0900

    pytest in vscode

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,10 +2,12 @@ import pytest
 from fastapi.testclient import TestClient
 
 from api.main import app
+import json
 
 client = TestClient(app)
 
 def test_check_main():
-    res = client.get("/")
+    res = client.get("/tasks")
     assert res.status_code == 200
-    assert res.json() == {"msg": "Hello world"}
+    assert res.json() != None
+    print(res.json())
